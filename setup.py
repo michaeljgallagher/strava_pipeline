ebabfda832170d646ac0df1a77fcf14e17da3cb6
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-06-22 21:20:32 -0400

    Update author, license, and url in setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name="strava_pipeline",
     version="0.1",
+    author="Michael Gallagher",
+    author_email="mjgall@pm.me",
+    maintainer="Michael Gallagher",
+    maintainer_email="mjgall@pm.me",
+    license="MIT",
+    url="https://github.com/michaeljgallagher/strava_pipeline",
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
     package_data={
