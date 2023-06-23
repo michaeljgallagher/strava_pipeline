6fb51d81fc405618f0365ef1867cf3a7045deaf9
Michael Gallagher <11604739+michaeljgallagher@users.noreply.github.com>
2023-06-23 17:04:59 -0400

    Add classifiers to setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,16 @@ setup(
     version="0.1",
     author="Michael Gallagher",
     author_email="mjgall@pm.me",
-    maintainer="Michael Gallagher",
-    maintainer_email="mjgall@pm.me",
     license="MIT",
     url="https://github.com/michaeljgallagher/strava_pipeline",
     packages=find_packages(),
     install_requires=open("requirements.txt").readlines(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
     package_data={
         "": ["pipeline.ini"],
     },
